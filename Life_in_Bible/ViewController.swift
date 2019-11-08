50c9a05fb5534df11fda5f19f61b6c6285ed9e18
vinodbhavya <formando@c2-lcm-m05.local>
2019-11-08 17:48:41 +0000

    first commit

---
+++ Life_in_Bible/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life_in_Bible
+//
+//  Created by Mohammad Saiful Kabir on 07/11/2019.
+//  Copyright © 2019 Mohammad Saiful Kabir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Life_in_BibleTests/Life_in_BibleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Life_in_BibleTests.swift
+//  Life_in_BibleTests
+//
+//  Created by Mohammad Saiful Kabir on 07/11/2019.
+//  Copyright © 2019 Mohammad Saiful Kabir. All rights reserved.
+//
+
+import XCTest
+@testable import Life_in_Bible
+
+class Life_in_BibleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
