b7fad0c43e51695ad86c31deb3cf59cb6df4902f
vinodbhavya <vinodbhavya410@gmail.com>
2019-11-21 18:33:47 +0000

    listing the books

--- Life_in_Bible/BooksTableViewController.swift
+++ Life_in_Bible/BooksTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class BooksTableViewController: UITableViewController {
     
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Life_in_Bible/Model/Book.swift
@@ -0,0 +1,28 @@
+//
+//  Books.swift
+//  Life_in_Bible
+//
+//  Created by bhavya on 14/11/19.
+//  Copyright © 2019 Mohammad Saiful Kabir. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class Book {
+    
+    var bookname: String = ""
+    var bookid: String = ""
+    var numberofchapters: Int = 0
+    
+init(bookname: String, bookid: String, numberofchapters: Int){
+    self.bookname = bookname
+    self.bookid = bookid
+    self.numberofchapters = numberofchapters
+    
+}
+}
+
+
+
+
+
