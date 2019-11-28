d897b3eb99df672be474697aeecac43cdfe4692e
vinodbhavya <vinodbhavya410@gmail.com>
2019-11-28 17:19:32 +0000

    added some methods

---
+++ Life_in_Bible/Custom cell/BookTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  BookTableViewCell.swift
+//  Life_in_Bible
+//
+//  Created by rajasekharreddy.talamanchi on 28/11/19.
+//  Copyright © 2019 Mohammad Saiful Kabir. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Life_in_Bible/ViewController.swift
+++ Life_in_Bible/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
- 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -22,18 +22,14 @@ class ViewController: UIViewController {
         if(segue.identifier == "OldTestamentSegue") {
             if let viewcontroller = segue.destination as? BooksTableViewController {
                 viewcontroller.dam_id = "ENGKJVO1DA"
-               
+                
             }
-            else if(segue.identifier == "NewTestamentSegue") {
-                if let viewcontroller = segue.destination as? BooksTableViewController {
-                    viewcontroller.dam_id = "ENGKJVN1DA"
-                }
+        } else if(segue.identifier == "NewTestamentSegue") {
+            if let viewcontroller = segue.destination as? BooksTableViewController {
+                viewcontroller.dam_id = "ENGKJVN1DA"
             }
-            
         }
         
-
-
     }
     /*
     // MARK: - Navigation
